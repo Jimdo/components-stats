ea315650ae207f96aa804b44205d17ee44975839
Stefania Mellai <stefania.mellai@jimdo.com>
2022-09-21 11:59:47 +0200

    chore: set up everything to have good npm package

--- src/cloneReposList.ts
+++ src/cloneReposList.ts
@@ -1,33 +1,49 @@
-var shell = require('shelljs');
-const fs = require('node:fs');
+import fs from 'fs';
 import { RelevantRepo } from 'package-adoption';
+import { cd, exec } from 'shelljs';
 
-export const cloneReposList = (reposLocalDir: string, repos: RelevantRepo[], org: string) => {
+/**
+ * It clones all the relevant repos from a given organization into a local directory
+ * @param {string} reposLocalDir - The directory where the repos will be cloned to.
+ * @param {RelevantRepo[]} repos - An array of relevant repos.
+ * @param {string} org - The name of the organization you want to get the stats for.
+ */
+export const cloneReposList = (
+  reposLocalDir: string,
+  repos: RelevantRepo[],
+  org: string
+): void => {
   if (fs.existsSync(reposLocalDir)) {
     // shell.rm('-rf', `${reposLocalDir}/*`);
   } else {
     fs.mkdirSync(reposLocalDir);
   }
-  shell.cd(reposLocalDir);
+  cd(reposLocalDir);
 
   for (let i = 0; i < repos.length; i++) {
-    const repo = repos[i];
+    const repo: RelevantRepo = repos[i];
     const repoURL = `git@github.com:${org}/${repo.name}.git`;
 
     const localRepoDir = `${reposLocalDir}/${repo.name}`;
     if (fs.existsSync(localRepoDir)) {
-      console.log('[components-stats] - %s directory already exists, updating it...', `${org}/${repo.name}`);
+      console.log(
+        '[components-stats] - %s directory already exists, updating it...',
+        `${org}/${repo.name}`
+      );
 
-      shell.cd(localRepoDir);
-      if (shell.exec(`git pull`).code !== 0) {
+      cd(localRepoDir);
+      if (exec('git pull').code !== 0) {
         console.error('Git pull failed for repo: %s', repo.name);
       }
-      shell.cd('..');
+      cd('..');
     } else {
       console.log('[components-stats] - Cloning %s...', `${org}/${repo.name}`);
 
-      if (shell.exec(`git clone ${repoURL}`).code !== 0) {
-        console.error('[components-stats] - Git clone failed for repo: %s', repo.name);
+      if (exec(`git clone ${repoURL}`).code !== 0) {
+        console.error(
+          '[components-stats] - Git clone failed for repo: %s',
+          repo.name
+        );
       }
     }
   }
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,93 @@
 // Inspired by https://www.twilio.com/blog/insights-metrics-inform-paste-design-system
 
-const shell = require('shelljs');
-const fs = require('node:fs');
-const path = require('node:path');
-const { cwd } = require('node:process');
-import { getFilteredReposWithPackageForOrg, RelevantRepo } from 'package-adoption';
+import { exec, rm } from 'shelljs';
+import fs from 'fs';
+import path from 'path';
+import { cwd } from 'process';
+import {
+  getFilteredReposWithPackageForOrg,
+  RelevantRepo,
+  Config,
+} from 'package-adoption';
+import { cloneReposList } from './cloneReposList';
 
-const { cloneReposList } = require('./cloneReposList');
+interface LocalConfig extends Config {
+  COMPONENTS: any;
+}
 
-const { ORG, PKG_NAME, GH_AUTHTOKEN, DAYS_UNTIL_STALE, COMPONENTS } = require('../config');
+const {
+  ORG,
+  PKG_NAME,
+  GH_AUTHTOKEN,
+  DAYS_UNTIL_STALE,
+  COMPONENTS,
+}: LocalConfig = require('../config');
 
 const currentLocation = cwd();
 const stanitizedPkgName = PKG_NAME.replaceAll('/', '_');
 const reposLocalDir = path.join(currentLocation, 'repositories');
 const pkgAdoptionReport = path.join(currentLocation, 'pkgAdoption.json');
-const reportsOutputDir = path.join(currentLocation, `reports_by_repo-${stanitizedPkgName}`);
-const reactScannerConfig = path.join(currentLocation, 'react-scanner.config.js');
+const reportsOutputDir = path.join(
+  currentLocation,
+  `reports_by_repo-${stanitizedPkgName}`
+);
+const reactScannerConfig = path.join(
+  currentLocation,
+  'react-scanner.config.js'
+);
 
 (async () => {
-  const relevantRepos: RelevantRepo[] | undefined = await getFilteredReposWithPackageForOrg(ORG, DAYS_UNTIL_STALE, GH_AUTHTOKEN, PKG_NAME);
+  const relevantRepos: RelevantRepo[] | undefined =
+    await getFilteredReposWithPackageForOrg({
+      org: ORG,
+      daysUntilStale: DAYS_UNTIL_STALE,
+      ghAuthToken: GH_AUTHTOKEN,
+      pkgName: PKG_NAME,
+    });
   fs.writeFileSync(pkgAdoptionReport, JSON.stringify(relevantRepos));
 
   if (relevantRepos?.length) {
     cloneReposList(reposLocalDir, relevantRepos, ORG);
 
-    console.log('\n\n[components-stats] - Collect components usage for filtered repositories');
+    console.log(
+      '\n\n[components-stats] - Collect components usage for filtered repositories'
+    );
 
-    shell.rm('-rf', `${reportsOutputDir}/*`);
+    rm('-rf', `${reportsOutputDir}/*`);
     for (let i = 0; i < relevantRepos.length; i++) {
       const repo = relevantRepos[i];
       const installationPath = repo.installationPath;
-      const sourceDir = installationPath === 'root' ? repo.name : path.join(repo.name, installationPath);
-      const baseReportFileName = `scanner-report_${repo.name}`; 
-      const reportFileName = installationPath === 'root' ? `${baseReportFileName}.json` : `${baseReportFileName}_${installationPath.replaceAll('/', '_')}.json`;
-      
+      const sourceDir =
+        installationPath === 'root'
+          ? repo.name
+          : path.join(repo.name, installationPath);
+      const baseReportFileName = `scanner-report_${repo.name}`;
+      const reportFileName =
+        installationPath === 'root'
+          ? `${baseReportFileName}.json`
+          : `${baseReportFileName}_${installationPath.replaceAll(
+              '/',
+              '_'
+            )}.json`;
+
       const scannerConfig = {
         crawlFrom: path.join(reposLocalDir, sourceDir),
         includeSubComponents: true,
         importedFrom: PKG_NAME,
         processors: [
-          ['count-components-and-props', { outputTo: path.join(reportsOutputDir, reportFileName) }],
+          [
+            'count-components-and-props',
+            { outputTo: path.join(reportsOutputDir, reportFileName) },
+          ],
         ],
         components: COMPONENTS,
       };
 
-      fs.writeFileSync(reactScannerConfig, `module.exports = ${JSON.stringify(scannerConfig)}`);
-      shell.exec(`npx react-scanner -c ${reactScannerConfig}`);
+      fs.writeFileSync(
+        reactScannerConfig,
+        `module.exports = ${JSON.stringify(scannerConfig)}`
+      );
+      exec(`npx react-scanner -c ${reactScannerConfig}`);
     }
   }
 })().catch(err => {
